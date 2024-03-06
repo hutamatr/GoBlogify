f0402efa64c1475ebcaf7a8646858226ecdd7930
hutamatr <hutamatr@gmail.com>
2024-03-07 00:23:33 +0700

    refactor: refactor type struct entity, response & request

--- cmd/go_blog/article_response.go
+++ cmd/go_blog/article_response.go
@@ -1,10 +1,16 @@
 package go_blog
 
+import "time"
+
 type ArticleResponse struct {
-	Id        int    `json:"id"`
-	Title     string `json:"title"`
-	Body      string `json:"body"`
-	Author    string `json:"author"`
-	Published bool   `json:"published"`
-	Category  string `json:"category"`
+	Id         int    `json:"id"`
+	Title      string `json:"title"`
+	Body       string `json:"body"`
+	Author     string `json:"author"`
+	Category   string `json:"category"`
+	Published  bool   `json:"published"`
+	Deleted    bool   `json:"deleted"`
+	Created_At time.Time `json:"created_at"`
+	Updated_At time.Time `json:"updated_at"`
+	Deleted_At time.Time `json:"deleted_at"`
 }
