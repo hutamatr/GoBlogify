e4412fcd598aacbeaf88ed020447b026d4b5f540
hutamatr <hutamatr@gmail.com>
2024-03-07 17:34:24 +0700

    feat: add article controller method

--- cmd/go_blog/article_controller.go
+++ cmd/go_blog/article_controller.go
@@ -2,6 +2,7 @@ package go_blog
 
 import (
 	"net/http"
+	"strconv"
 
 	"github.com/hutamatr/go-blog-api/cmd/go_blog/helper"
 	"github.com/julienschmidt/httprouter"
@@ -29,6 +30,68 @@ func (controller *ArticleControllerImpl) CreateArticle(writer http.ResponseWrite
 		Code:   http.StatusCreated,
 		Status: "OK",
 		Data:   article,
+	}
+
+	helper.EncodeJSONFromResponse(writer, articleResponse)
+}
+
+func (controller *ArticleControllerImpl) FindAllArticle(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	articles := controller.service.FindAll(request.Context())
+
+	articleResponse := ResponseJSON{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   articles,
+	}
+
+	helper.EncodeJSONFromResponse(writer, articleResponse)
+}
+
+func (controller *ArticleControllerImpl) FindByIdArticle(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	id := params.ByName("articleId")
+	articleId, err := strconv.Atoi(id)
+
+	helper.PanicError(err)
+
+	article := controller.service.FindById(request.Context(), articleId)
+
+	articleResponse := ResponseJSON{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   article,
+	}
+
+	helper.EncodeJSONFromResponse(writer, articleResponse)
+}
+
+func (controller *ArticleControllerImpl) UpdateArticle(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	articleUpdateRequest := ArticleUpdateRequest{}
+
+	helper.DecodeJSONFromRequest(request, &articleUpdateRequest)
+
+	updatedArticle := controller.service.Update(request.Context(), articleUpdateRequest)
+
+	articleResponse := ResponseJSON{
+		Code:   http.StatusOK,
+		Status: "OK",
+		Data:   updatedArticle,
+	}
+
+	helper.EncodeJSONFromResponse(writer, articleResponse)
+}
+
+func (controller *ArticleControllerImpl) DeleteArticle(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+
+	id := params.ByName("articleId")
+
+	articleId, err := strconv.Atoi(id)
+	helper.PanicError(err)
+
+	controller.service.Delete(request.Context(), articleId)
+
+	articleResponse := ResponseJSON{
+		Code:   http.StatusOK,
+		Status: "OK",
 	}
 
 	helper.EncodeJSONFromResponse(writer, articleResponse)
