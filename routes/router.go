34043c1f28bec3da5f68e8adcd25ba076a91499f
hutamatr <hutamatr@gmail.com>
2024-03-18 18:02:46 +0700

    feat: add role router

--- routes/router.go
+++ routes/router.go
@@ -5,6 +5,7 @@ import (
 
 	controllersA "github.com/hutamatr/go-blog-api/controllers/article"
 	controllersC "github.com/hutamatr/go-blog-api/controllers/category"
+	controllersR "github.com/hutamatr/go-blog-api/controllers/role"
 	controllersU "github.com/hutamatr/go-blog-api/controllers/user"
 	"github.com/hutamatr/go-blog-api/exception"
 	"github.com/hutamatr/go-blog-api/helpers"
@@ -16,6 +17,7 @@ type RouterControllers struct {
 	UserController     controllersU.UserController
 	ArticleController  controllersA.ArticleController
 	CategoryController controllersC.CategoryController
+	RoleController     controllersR.RoleController
 }
 
 func Router(routerControllers *RouterControllers) *httprouter.Router {
@@ -29,15 +31,21 @@ func Router(routerControllers *RouterControllers) *httprouter.Router {
 	router.DELETE("/api/user/:userId", routerControllers.UserController.DeleteUser)
 	router.GET("/api/refresh", routerControllers.UserController.GetRefreshToken)
 
+	router.POST("/api/role", routerControllers.RoleController.CreateRole)
+	router.GET("/api/role", routerControllers.RoleController.FindAllRole)
+	router.GET("/api/role/:roleId", routerControllers.RoleController.FindRoleById)
+	router.PUT("/api/role/:roleId", routerControllers.RoleController.UpdateRole)
+	router.DELETE("/api/role/:roleId", routerControllers.RoleController.DeleteRole)
+
+	router.POST("/api/article", routerControllers.ArticleController.CreateArticle)
 	router.GET("/api/article", routerControllers.ArticleController.FindAllArticle)
 	router.GET("/api/article/:articleId", routerControllers.ArticleController.FindByIdArticle)
-	router.POST("/api/article", routerControllers.ArticleController.CreateArticle)
 	router.PUT("/api/article/:articleId", routerControllers.ArticleController.UpdateArticle)
 	router.DELETE("/api/article/:articleId", routerControllers.ArticleController.DeleteArticle)
 
+	router.POST("/api/category", routerControllers.CategoryController.CreateCategory)
 	router.GET("/api/category", routerControllers.CategoryController.FindAllCategory)
 	router.GET("/api/category/:categoryId", routerControllers.CategoryController.FindByIdCategory)
-	router.POST("/api/category", routerControllers.CategoryController.CreateCategory)
 	router.PUT("/api/category/:categoryId", routerControllers.CategoryController.UpdateCategory)
 	router.DELETE("/api/category/:categoryId", routerControllers.CategoryController.DeleteCategory)
 
