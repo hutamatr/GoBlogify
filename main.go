f8e7f3c76e936792160320964fbeeeb2124713bd
hutamatr <hutamatr@gmail.com>
2024-03-18 18:03:21 +0700

    feat: add role repository, service, controller constructors func to main

--- main.go
+++ main.go
@@ -9,14 +9,17 @@ import (
 	"github.com/hutamatr/go-blog-api/app"
 	controllersA "github.com/hutamatr/go-blog-api/controllers/article"
 	controllersC "github.com/hutamatr/go-blog-api/controllers/category"
+	controllersR "github.com/hutamatr/go-blog-api/controllers/role"
 	controllersU "github.com/hutamatr/go-blog-api/controllers/user"
 	"github.com/hutamatr/go-blog-api/helpers"
 	repositoriesA "github.com/hutamatr/go-blog-api/repositories/article"
 	repositoriesC "github.com/hutamatr/go-blog-api/repositories/category"
+	repositoriesR "github.com/hutamatr/go-blog-api/repositories/role"
 	repositoriesU "github.com/hutamatr/go-blog-api/repositories/user"
 	"github.com/hutamatr/go-blog-api/routes"
 	servicesA "github.com/hutamatr/go-blog-api/services/article"
 	servicesC "github.com/hutamatr/go-blog-api/services/category"
+	servicesR "github.com/hutamatr/go-blog-api/services/role"
 	servicesU "github.com/hutamatr/go-blog-api/services/user"
 
 	"github.com/joho/godotenv"
@@ -44,6 +47,10 @@ func main() {
 	userService := servicesU.NewUserService(userRepository, db, validator)
 	UserController := controllersU.NewUserController(userService)
 
+	roleRepository := repositoriesR.NewRoleRepository()
+	roleService := servicesR.NewRoleService(roleRepository, db, validator)
+	roleController := controllersR.NewRoleController(roleService)
+
 	articleRepository := repositoriesA.NewArticleRepository()
 	articleService := servicesA.NewArticleService(articleRepository, db, validator)
 	articleController := controllersA.NewArticleController(articleService)
@@ -56,6 +63,7 @@ func main() {
 		UserController:     UserController,
 		ArticleController:  articleController,
 		CategoryController: categoryController,
+		RoleController:     roleController,
 	})
 
 	cors := helpers.Cors()
