d988896163f72635fcc4dfa9fc4242f6990f5846
hutamatr <hutamatr@gmail.com>
2024-03-22 17:00:36 +0700

    feat: create comment repository & implementation method

---
+++ repositories/comment/comment_repository.go
@@ -0,0 +1,16 @@
+package repositories
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/hutamatr/GoBlogify/model/domain"
+)
+
+type CommentRepository interface {
+	Save(ctx context.Context, tx *sql.Tx, comment domain.Comment) domain.CommentJoin
+	FindCommentsByPost(ctx context.Context, tx *sql.Tx, postId int, offset int) []domain.CommentJoin
+	FindById(ctx context.Context, tx *sql.Tx, commentId int) domain.CommentJoin
+	Update(ctx context.Context, tx *sql.Tx, comment domain.Comment) domain.CommentJoin
+	Delete(ctx context.Context, tx *sql.Tx, commentId int)
+}
