5f46b4ad8ddce847233d00238f626b3805acaf5e
hutamatr <hutamatr@gmail.com>
2024-04-26 06:44:34 +0700

    feat: create count-follower & count-following repository & used on follow service

--- follow/repository.go
+++ follow/repository.go
@@ -14,12 +14,14 @@ type FollowRepositories interface {
 	FindAllFollowedByUser(ctx context.Context, tx *sql.Tx, followerId, limit, offset int) []FollowJoin
 	FindById(ctx context.Context, tx *sql.Tx, followId int) Follow
 	Delete(ctx context.Context, tx *sql.Tx, followerId, followedId int)
+	CountFollower(ctx context.Context, tx *sql.Tx, followedId int) int
+	CountFollowed(ctx context.Context, tx *sql.Tx, followerId int) int
 }
 
 type FollowRepositoriesImpl struct {
 }
 
-func NewFollowRepository() *FollowRepositoriesImpl {
+func NewFollowRepository() FollowRepositories {
 	return &FollowRepositoriesImpl{}
 }
 
@@ -40,7 +42,11 @@ func (repository *FollowRepositoriesImpl) Save(ctx context.Context, tx *sql.Tx, 
 }
 
 func (repository *FollowRepositoriesImpl) FindAllFollowerByUser(ctx context.Context, tx *sql.Tx, followedId, limit, offset int) []FollowJoin {
-	query := "SELECT user.id, user.username, user.first_name, user.last_name, follow.id, follow.followed_id, follow.follower_id, follow.created_at, follow.updated_at FROM follow JOIN user ON follow.follower_id = user.id WHERE follow.followed_id = ? LIMIT ? OFFSET ?"
+	query := `SELECT u.id, u.username, u.first_name, u.last_name, f.id, f.followed_id, f.follower_id, f.created_at, f.updated_at 
+	FROM user u 
+	JOIN follow f 
+	ON u.id = f.follower_id 
+	WHERE f.followed_id = ? LIMIT ? OFFSET ?`
 
 	rows, err := tx.QueryContext(ctx, query, followedId, limit, offset)
 
@@ -76,7 +82,11 @@ func (repository *FollowRepositoriesImpl) FindAllFollowerByUser(ctx context.Cont
 }
 
 func (repository *FollowRepositoriesImpl) FindAllFollowedByUser(ctx context.Context, tx *sql.Tx, followerId, limit, offset int) []FollowJoin {
-	query := "SELECT user.id, user.username, user.first_name, user.last_name, follow.id, follow.followed_id, follow.follower_id, follow.created_at, follow.updated_at FROM follow JOIN user ON follow.followed_id = user.id WHERE follow.follower_id = ? LIMIT ? OFFSET ?"
+	query := `SELECT u.id, u.username, u.first_name, u.last_name, f.id, f.followed_id, f.follower_id, f.created_at, f.updated_at 
+	FROM user u 
+	JOIN follow f 
+	ON u.id = f.followed_id 
+	WHERE f.follower_id = ? LIMIT ? OFFSET ?`
 
 	rows, err := tx.QueryContext(ctx, query, followerId, limit, offset)
 
@@ -112,7 +122,7 @@ func (repository *FollowRepositoriesImpl) FindAllFollowedByUser(ctx context.Cont
 }
 
 func (repository *FollowRepositoriesImpl) FindById(ctx context.Context, tx *sql.Tx, followId int) Follow {
-	query := "SELECT id, follower_id, followed_id FROM follow WHERE id = ?"
+	query := "SELECT id, follower_id, followed_id, created_at, updated_at FROM follow WHERE id = ?"
 
 	rows, err := tx.QueryContext(ctx, query, followId)
 
@@ -123,17 +133,17 @@ func (repository *FollowRepositoriesImpl) FindById(ctx context.Context, tx *sql.
 	var follow Follow
 
 	if rows.Next() {
-		err := rows.Scan(&follow.Id, &follow.Follower_Id, &follow.Followed_Id)
+		err := rows.Scan(&follow.Id, &follow.Follower_Id, &follow.Followed_Id, &follow.Created_At, &follow.Updated_At)
 		helpers.PanicError(err, "failed to scan follow by id")
 	}
 
 	return follow
 }
 
-func (repository *FollowRepositoriesImpl) Delete(ctx context.Context, tx *sql.Tx, followerId, userId int) {
+func (repository *FollowRepositoriesImpl) Delete(ctx context.Context, tx *sql.Tx, followerId, followedId int) {
 	query := "DELETE FROM follow WHERE follower_id = ? AND followed_id = ?"
 
-	result, err := tx.ExecContext(ctx, query, followerId, userId)
+	result, err := tx.ExecContext(ctx, query, followerId, followedId)
 	helpers.PanicError(err, "failed to exec query delete follow")
 
 	resultRows, err := result.RowsAffected()
@@ -142,4 +152,40 @@ func (repository *FollowRepositoriesImpl) Delete(ctx context.Context, tx *sql.Tx
 	if resultRows == 0 {
 		panic(exception.NewNotFoundError("follow not found"))
 	}
+}
+
+func (repository *FollowRepositoriesImpl) CountFollower(ctx context.Context, tx *sql.Tx, followedId int) int {
+	query := "SELECT COUNT(*) FROM follow WHERE followed_id = ?"
+
+	rows, err := tx.QueryContext(ctx, query, followedId)
+	helpers.PanicError(err, "failed to exec query count follower")
+
+	defer rows.Close()
+
+	var countFollower int
+
+	if rows.Next() {
+		err := rows.Scan(&countFollower)
+		helpers.PanicError(err, "failed to scan count follower")
+	}
+
+	return countFollower
+}
+
+func (repository *FollowRepositoriesImpl) CountFollowed(ctx context.Context, tx *sql.Tx, followerId int) int {
+	query := "SELECT COUNT(*) FROM follow WHERE follower_id = ?"
+
+	rows, err := tx.QueryContext(ctx, query, followerId)
+	helpers.PanicError(err, "failed to exec query count followed")
+
+	defer rows.Close()
+
+	var countFollowed int
+
+	if rows.Next() {
+		err := rows.Scan(&countFollowed)
+		helpers.PanicError(err, "failed to scan count followed")
+	}
+
+	return countFollowed
 }
